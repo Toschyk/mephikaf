5e50baef6ed36f995e9ad0cc422ce8ec1463bacc
Toschyk <toschyk@icloud.com>
2023-03-24 16:25:40 +0700

    config cookie and db

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '7875i75gi575i75875g8i5g8',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
