38b8e15c32d9e26734a1448d3ae4152b22337d14
Toschyk <toschyk@icloud.com>
2023-03-26 16:43:43 +0700

    Separation of access rights

---
+++ views/admin/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Административная панель';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Управление пользователями', ['/user']) ?>
+    </p>
+</div>
